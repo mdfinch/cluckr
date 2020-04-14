f077538d7e27d08d6707eb5b8345c38397fed702
Michael Finch <michael.donovan.finch@gmail.com>
2020-04-14 10:45:22 -0700

    add penguin icon, add sign in on welcome page

--- db/migrations/20200414094026_CreateClucks.js
+++ db/migrations/20200414094026_CreateClucks.js
@@ -1,14 +1,13 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
     return knex.schema.createTable("clucks", table => {
-      table.increments("id");
-      table.string("image_url")
-      table.text("content");
-      table.timestamps();
+        table.increments("id");
+        table.string("image_url")
+        table.text("content");
+        table.timestamps(); // This creates both created_at and updated_at columns
     });
-  };
-  
-  exports.down = function(knex) {
-      return knex.schema.dropTable("clucks");
-  };
-  
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("clucks");
+};
